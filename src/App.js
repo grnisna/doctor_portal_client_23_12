9138aa5b3bb886cecb0661d6837bc39f43c1b5a8
grnisna <mdnisanahmed63@gmail.com>
2022-12-23 11:37:18 +0600

    new comment

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import logo from "./logo.svg";
+
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/Page/About";
+import Appointment from "./Pages/Page/Appointment";
+import Contacts from "./Pages/Page/Contacts";
+import Home from "./Pages/Page/Home";
+import Reviews from "./Pages/Page/Reviews";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <div class="text-center lg:text-left">
-      <h1 class="text-5xl font-bold">Login now!</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div class="card-body">
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" class="input input-bordered" />
-        </div>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" class="input input-bordered" />
-          <label class="label">
-            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div class="form-control mt-6">
-          <button class="btn btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About/>} ></Route>
+        <Route path="/Contacts" element={<Contacts/>} ></Route>
+        <Route path="/Appointment" element={<Appointment/>} ></Route>
+        <Route path="/Reviews" element={<Reviews/>} ></Route>
+      </Routes>
     </div>
   );
 }
